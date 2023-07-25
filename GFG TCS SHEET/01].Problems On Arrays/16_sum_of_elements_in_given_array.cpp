f5e310c974f292172902a91e153703e29c9bfd0d
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-26 00:13:00 +0530

    array/GFG

---
+++ GFG TCS SHEET/01].Problems On Arrays/16_sum_of_elements_in_given_array.cpp
@@ -0,0 +1,15 @@
+//User function Template for C++
+
+//Complete this function
+
+int sumElement(int arr[],int n)
+{
+    //Your code here
+    int count=0;
+    for(int i=0;i<n;i++){
+        count=count+arr[i];
+        
+    }
+    return count;
+}
+
