8aab97983b7ec9426c25d94b789701cb8ec4774e
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-26 12:26:30 +0530

    array/GFG

---
+++ GFG TCS SHEET/01].Problems On Arrays/18_program_to_check_if_array_is_sorted_or_not.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+  
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
