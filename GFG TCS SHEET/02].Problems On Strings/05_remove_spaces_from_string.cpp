7c0d93d2e1616b9547c28a30bd0d45904f62b70f
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-27 16:44:16 +0530

    learned about s.substr function

--- GFG TCS SHEET/02].Problems On Strings/05_remove_spaces_from_string.cpp
+++ GFG TCS SHEET/02].Problems On Strings/05_remove_spaces_from_string.cpp
@@ -4,13 +4,24 @@ class Solution
     string modify (string s)
     {
         //code here.
-        string temp="";
-        for(int i=0;i<s.size();i++){
+        // string temp="";
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]!=' '){
+        //         temp=temp+s[i];
+        //     }
+        // }
+        // s=temp;
+        // return s;
+        //no extra space
+        int i=0,itr=0;
+        int n=s.size();
+        while(i<n){
             if(s[i]!=' '){
-                temp=temp+s[i];
+                s[itr++]=s[i];
+                
             }
+            i++;
         }
-        s=temp;
-        return s;
+        return s.substr(0,itr);
     }
 };
