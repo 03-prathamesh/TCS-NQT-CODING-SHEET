d06b00fd6ecad7418baf8927dc631f0426e84c19
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-28 15:00:44 +0530

    used my best approach of removing elements using itr but this type with the help of unordered map

--- GFG TCS SHEET/02].Problems On Strings/06_remove_characters.cpp
+++ GFG TCS SHEET/02].Problems On Strings/06_remove_characters.cpp
@@ -3,20 +3,32 @@ class Solution {
   public:
     string removeChars(string string1, string string2) {
         // code here
-        int hash[257];
-        string result="";
-        for(int i=0;i<257;i++){
-            hash[i]=0;
-        }
+        // int hash[257];
+        // string result="";
+        // for(int i=0;i<257;i++){
+        //     hash[i]=0;
+        // }
+        // for(int i=0;i<string2.size();i++){
+        //     hash[string2[i]]++;
+        // }
+        // for(int i=0;i<string1.size();i++){
+        //     if(hash[string1[i]]==0){
+        //         result=result+string1[i];
+        //     }
+        // }   //this will also run .
+        // string1=result;
+        // return string1;
+        unordered_map<char,int>mp;
         for(int i=0;i<string2.size();i++){
-            hash[string2[i]]++;
+            mp[string2[i]]++;
         }
+        int iter=0;
         for(int i=0;i<string1.size();i++){
-            if(hash[string1[i]]==0){
-                result=result+string1[i];
+            if(mp.find(string1[i])==mp.end()){
+                string1[iter]=string1[i];
+                iter++;
             }
         }
-        string1=result;
-        return string1;
+        return string1.substr(0,iter);
     }
 };
