7a0fb21facdde05a4212ad15c591f155a49ead7a
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-28 14:15:49 +0530

    One of the best approach ever for removing element by dry running and debugging the test case

--- GFG TCS SHEET/02].Problems On Strings/04_remove_wovels_from_string.cpp
+++ GFG TCS SHEET/02].Problems On Strings/04_remove_wovels_from_string.cpp
@@ -2,14 +2,25 @@
 class Solution{
 public:	
 		
+	class Solution{
+public:	
+		
 	string removeVowels(string S) 
 	{
 	    // Your code goes here
-	    for(int i=S.size()-1;i>=0;i--){
-	        if(S[i]=='a'|| S[i]=='e' || S[i]=='i' || S[i]=='o' || S[i]=='u' ){
-	            S.erase(i,1);
-	        }
-	    }
-	    return S;
+	   // for(int i=S.size()-1;i>=0;i--){
+	   //     if(S[i]=='a'|| S[i]=='e' || S[i]=='i' || S[i]=='o' || S[i]=='u' ){
+	   //         S.erase(i,1);
+	   //     }
+	   // } // this will also work accurately
+	   int itr=0;
+	   for(int i=0;i<S.size();i++){
+	       if(S[i]!='a' &&  S[i]!='e' && S[i]!='i' && S[i]!='o' && S[i]!='u' ){
+	           S[itr++]=S[i];
+	           
+	       }
+	   }
+	    return S.substr(0,itr);
 	}
+};
 };
