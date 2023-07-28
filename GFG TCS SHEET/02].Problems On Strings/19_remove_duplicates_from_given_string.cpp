a1e884814c862d09c6a2e8a030b027e18ae667b9
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-07-28 21:44:08 +0530

    used of find function and npos + iterator syntax in string . very best and efficient approach ever

--- GFG TCS SHEET/02].Problems On Strings/19_remove_duplicates_from_given_string.cpp
+++ GFG TCS SHEET/02].Problems On Strings/19_remove_duplicates_from_given_string.cpp
@@ -7,11 +7,22 @@ public:
 	   temp.push_back(str[0]);
 	   for(int i=0;i<str.size();i++){
 	       bool isdupl=false;
-	       for(int j=0;j<temp.size();j++){
-	           if(temp[j]==str[i]){
-	               isdupl=true;
-	               break;
-	           }
+	       //for(int j=0;j<temp.size();j++){
+	       //    //if(temp[j]==str[i]){
+	       //    //    isdupl=true;
+	       //    //    break;
+	       //    //}
+	       //    
+	       //}
+	       //size_t it=temp.find(str[i]);
+	       //if(it!=string::npos){
+	       //    isdupl=true;
+	           
+	       //}      alternative syntax for the npos .......
+	       string::iterator it=find(temp.begin(),temp.end(),str[i]);
+	       if(it!=temp.end()){
+	           isdupl=true;
+	           
 	       }
 	       if(!isdupl){
 	            temp.push_back(str[i]);
